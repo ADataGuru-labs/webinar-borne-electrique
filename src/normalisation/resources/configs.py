40f5e907dc7d999f0783b13c24a63be6bda376b4
lcaminale <loic-external.caminale@dalkia.fr>
2022-11-18 10:29:55 +0100

    update: augmentation du nombre de lignes

--- src/normalisation/resources/configs.py
+++ src/normalisation/resources/configs.py
@@ -4,7 +4,7 @@ env = os.environ.get("ENV", "test")
 configs = {
     "s3_path": ("s3://bornes-electriques-webinar/%s/bornes_electriques.parquet" % env),
     "open_data_api": {
-        "nbr_rows": 100,
+        "nbr_rows": 1000,
         "base_name": "opendata.paris.fr/api/records/1.0/search/",
         "dataset_name": "belib-points-de-recharge-pour-vehicules-electriques-disponibilite-temps-reel&q",
     },
